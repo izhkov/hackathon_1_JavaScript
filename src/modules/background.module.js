8cf6d97895961e20b44f99ba3f1a338ecd3fc0b9
Ivan Izhkov <ivan.izhkov@mail.ru>
2023-05-13 18:51:24 +0500

    modified styles.css

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,3 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
-export class BackgroundModule extends Module {
-
-}
+export class BackgroundModule extends Module {}
