cc59222db84bc4b536705f93fd5d33af06098164
Valentin <hanolo@yandex.ru>
2023-05-13 13:48:28 +0300

    message.module

---
+++ src/modules/messages.module.js
@@ -0,0 +1,17 @@
+
+export class MessagesModule {
+    constructor() {
+        this.content = ['Мы знаем, что любим кого-то таким какой он есть, но не можем знать, полюбили бы мы его, если он был другим.', 'Если тебе тяжело, значит ты поднимаешься в гору. Если тебе легко, значит ты летишь в пропасть.', 'Не стоит принимать доброту за слабость, грубость за силу, а подлость за умение жить.', 'Никто не ценит того, чего слишком много.', 'Невозможно победить того, кто не сдается.', 'Одна из самых трудных в жизни вещей — хранить в сердце слова, которые нельзя произносить.',];
+    }
+    //Появление кастомного сообщения
+    block() {
+        let customBlock = document.createElement("div");
+        customBlock.className = "custom-block";
+        const message = document.createElement("p");
+        message.className = "message";
+        message.textContent = this.content[Math.floor(Math.random() * this.content.length)];
+        customBlock.append(message);
+        document.body.append(customBlock);
+        setTimeout(() => {document.body.innerHTML = ""},5000)
+    }
+}
