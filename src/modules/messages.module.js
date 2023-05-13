182af741944a330e5c66665eb65fa04e658a0e4c
Valentin <hanolo@yandex.ru>
2023-05-13 14:55:10 +0300

    message.module v.2

--- src/modules/messages.module.js
+++ src/modules/messages.module.js
@@ -1,10 +1,13 @@
+import {Module} from '../core/module'
 
-export class MessagesModule {
-    constructor() {
+export class MessagesModule extends Module {
+    constructor(type, text) {
+        super(type, text)
+        
         this.content = ['Мы знаем, что любим кого-то таким какой он есть, но не можем знать, полюбили бы мы его, если он был другим.', 'Если тебе тяжело, значит ты поднимаешься в гору. Если тебе легко, значит ты летишь в пропасть.', 'Не стоит принимать доброту за слабость, грубость за силу, а подлость за умение жить.', 'Никто не ценит того, чего слишком много.', 'Невозможно победить того, кто не сдается.', 'Одна из самых трудных в жизни вещей — хранить в сердце слова, которые нельзя произносить.',];
     }
     //Появление кастомного сообщения
-    block() {
+    trigger() {
         let customBlock = document.createElement("div");
         customBlock.className = "custom-block";
         const message = document.createElement("p");
@@ -12,6 +15,6 @@ export class MessagesModule {
         message.textContent = this.content[Math.floor(Math.random() * this.content.length)];
         customBlock.append(message);
         document.body.append(customBlock);
-        setTimeout(() => {document.body.innerHTML = ""},5000)
+        setTimeout(() => {customBlock.style.display = 'none'},5000)
     }
 }
