4a36bc6c8384f3d2b1cd50a02ffcff67750108a9
Anton Krat <threesundaysonewerk@gmail.com>
2023-05-13 23:57:45 +0800

    removing any inconsistencies with the repository

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
-import './styles.css'
-import { ContextMenu } from './menu'
-import { welcomeMessage } from './html'
-import { TimerModule } from './modules/timer.module'
+import "./styles.css";
+import { ContextMenu } from "./menu";
+import { welcomeMessage } from "./html";
+import { TimerModule } from "./modules/timer.module";
+import { ShapeModule } from "./modules/shape.module";
 
-const contextMenu = new ContextMenu('#menu')
-const timerModule = new TimerModule('timer', 'Таймер отсчета')
+const contextMenu = new ContextMenu("#menu");
+const timerModule = new TimerModule("timer", "Таймер отсчета");
 
-welcomeMessage()
+// Для теста shapeModule
+const shapeModule = new ShapeModule("shape", "Создать фигуру");
+shapeModule.trigger();
 
-const modules = []
+welcomeMessage();
 
-modules.push(timerModule)
+const modules = [];
+
+modules.push(timerModule);
 modules.forEach((module) => {
-  contextMenu.add(module)
-})
+  contextMenu.add(module);
+});
 
-contextMenu.open()
-contextMenu.close()
+contextMenu.open();
+contextMenu.close();
 
-document.addEventListener('click', (event) => {
-  const { target } = event
+document.addEventListener("click", (event) => {
+  const { target } = event;
 
-  if (target.dataset.type === 'timer') {
-    timerModule.trigger()
+  if (target.dataset.type === "timergit") {
+    timerModule.trigger();
   }
-})
+});
