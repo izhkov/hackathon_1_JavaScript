8a53c52b2f3a99775c80ced6622481fd043b33de
Xtramemium <93661341+Xtramemium@users.noreply.github.com>
2023-05-13 18:10:21 +0300

    background changes created

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,22 @@
-import {Module} from '../core/module'
+import { Module } from '@/core/module'
 
 export class BackgroundModule extends Module {
+    constructor() {
+        super('background-module', 'Изменить фон');
+    }
+    trigger() {
+        document.body.style.background = this.#randomColor()
+    }
+
+    // Генерация случайного цвета
+    #randomColor() {
+        const letters = '0123456789ABCDEF'
+        let color = '#'
+        while (color.length < 7) {
+            color += letters[Math.floor(Math.random() * letters.length)]
+        }
+        return color
+    }
 
 }
+
