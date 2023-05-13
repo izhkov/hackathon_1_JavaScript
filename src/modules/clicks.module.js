e7047a4868985faf81e4ee4ff754863d13d5e812
Mariia Dvoriankina <morfielmm@gmail.com>
2023-05-13 14:54:42 +0200

    update clicks module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,13 +1,17 @@
 import { Module } from '../core/module';
 
+import { ClicksCircles } from '../controllers/clicksCircles';
+
 export class ClicksModule extends Module {
-    constructor(type, text, time = 5000) {
+    constructor(type, text, time = 20000) {
         super(type, text);
 
         this.singleClickCount = 0;
         this.doubleClickCount = 0;
+        this.prepareTimerCount = 3;
         this.timeForAnalytics = time;
         this.isTriggered = false;
+        this.clicksCircles = new ClicksCircles();
 
         document.addEventListener('click', this.#increaseSingleClickCount.bind(this));
         document.addEventListener('dblclick', this.#increaseDoubleClickCount.bind(this));
@@ -15,27 +19,32 @@ export class ClicksModule extends Module {
 
     // Увеличивает счетчик в зависимости от типа клика
     #increaseCount(clickType) {
-        if (this.isTriggered) {
-            switch (clickType) {
-                case 'click':
-                    this.singleClickCount++;
-                    break;
-                case 'dblclick':
-                    this.singleClickCount -= 2;
-                    this.doubleClickCount++;
-                    break;
-            }
+
+        switch (clickType) {
+            case 'click':
+                this.singleClickCount++;
+                break;
+            case 'dblclick':
+                this.singleClickCount -= 2;
+                this.doubleClickCount++;
+                break;
         }
     }
 
     // Вызывает увеличение счетчика для одиночного клика
-    #increaseSingleClickCount() {
-        this.#increaseCount('click');
+    #increaseSingleClickCount(event) {
+        if (this.isTriggered) {
+            this.#increaseCount('click');
+            this.clicksCircles.createCircle('click', event.clientX, event.clientY);
+        }
     }
 
     // Вызывает увеличение счетчика для двойного клика
-    #increaseDoubleClickCount() {
-        this.#increaseCount('dblclick');
+    #increaseDoubleClickCount(event) {
+        if (this.isTriggered) {
+            this.#increaseCount('dblclick');
+            this.clicksCircles.createCircle('dblclick', event.clientX, event.clientY);
+        }
     }
 
     // Генерирует отчет с количеством кликов 
@@ -49,6 +58,39 @@ export class ClicksModule extends Module {
         this.#showAnalytics();
     }
 
+    #startAnalytics() {
+        this.prepareTimerElement.remove();
+
+        this.isTriggered = true;
+
+        this.timerUntilStop = setTimeout(this.#onTimeoutDown.bind(this), this.timeForAnalytics);
+
+    }
+
+    #createPrepareTimer() {
+        this.prepareTimerElement = document.createElement('div');
+        this.prepareTimerElement.classList.add('clicks-start-timer');
+
+        document.body.appendChild(this.prepareTimerElement);
+    }
+
+    #startPrepareTimer() {
+        this.prepareTimerElement.textContent = this.prepareTimerCount--;
+
+        const timer = setInterval(() => {
+            this.prepareTimerElement.textContent = this.prepareTimerCount ? this.prepareTimerCount : 'Начали';
+
+            if (!this.prepareTimerCount) {
+                setTimeout(() => {
+                    clearInterval(timer);
+                    this.#startAnalytics();
+                }, 600);
+            }
+
+            this.prepareTimerCount--;
+        }, 1000);
+    }
+
     // Входная точка основной метод модуля
     trigger() {
         if (this.timerUntilStop) {
@@ -59,13 +101,15 @@ export class ClicksModule extends Module {
                 return;
             }
         }
+        this.clicksCircles.removeCircles()
+
+        this.#createPrepareTimer();
+        this.#startPrepareTimer();
 
         this.singleClickCount = 0;
         this.doubleClickCount = 0;
 
-        this.isTriggered = true;
 
-        this.timerUntilStop = setTimeout(this.#onTimeoutDown.bind(this), this.timeForAnalytics);
     }
 }
 
