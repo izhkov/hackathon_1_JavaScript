e34fd1025f6fd605fecaa9921ed77deb631cd670
Ivan Izhkov <ivan.izhkov@mail.ru>
2023-05-13 20:17:22 +0500

    modified

--- src/app.js
+++ src/app.js
@@ -3,17 +3,23 @@ import { ContextMenu } from './menu'
 import { welcomeMessage } from './html'
 import { TimerModule } from './modules/timer.module'
 import { ClicksModule } from './modules/clicks.module'
+import { MessagesModule } from './modules/messages.module'
+import { ShapeModule } from './modules/shape.module'
 
 const contextMenu = new ContextMenu('#menu')
 const timerModule = new TimerModule('timer', 'Таймер отсчета')
-const clicksModule = new ClicksModule('clicks', 'Аналитика кликов (20 секунд на рисование кружками)')
+const clicksModule = new ClicksModule(
+  'clicks',
+  'Аналитика кликов (20 секунд на рисование кружками)'
+)
+const messagesModule = new MessagesModule('messages', 'Кастомное сообщение')
+const shapedModule = new ShapeModule('shape', 'Случайная фигура')
 
 welcomeMessage()
 
 const modules = []
 
-modules.push(timerModule)
-modules.push(clicksModule)
+modules.push(timerModule, clicksModule, messagesModule, shapedModule)
 modules.forEach((module) => {
   contextMenu.add(module)
 })
@@ -29,5 +35,11 @@ document.addEventListener('click', (event) => {
   }
   if (target.dataset.type === 'clicks') {
     clicksModule.trigger()
+  }
+  if (target.dataset.type === 'messages') {
+    messagesModule.trigger()
+  }
+  if (target.dataset.type === 'shape') {
+    shapedModule.trigger()
   }
 })
